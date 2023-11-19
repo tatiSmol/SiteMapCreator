45319be93b453a32e7091f527f941c82ad4102ce
tatiSmol <judeclapton@icloud.com>
2023-11-20 01:00:56 +0300

    Main class optimization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,44 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    private static final String ROOT_LINK = "https://lenta.ru/";
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN_UNDERLINED = "\033[4;32m";
     private static final String FOLDER_PATH = "data";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the website address in format like " +
+                            GREEN_UNDERLINED + "https://example.com/" + RESET + ":");
+        String rootLink = scanner.nextLine();
+
+        try {
+            new URL(rootLink);
+        } catch (MalformedURLException e) {
+            System.out.println("The entered URL is invalid. Please try again later.");
+            return;
+        }
+
         Path folderPath = Paths.get(FOLDER_PATH);
         try {
             if (!Files.exists(folderPath)) {
                 Files.createDirectories(folderPath);
             }
 
-            Node node = new Node(ROOT_LINK);
+            Node node = new Node(rootLink);
             try (ForkJoinPool pool = new ForkJoinPool()) {
                 pool.invoke(new MapCreator(node));
             }
 
-            try (FileWriter writer = new FileWriter(FOLDER_PATH + "/map.txt")) {
+            String siteName = rootLink.substring(rootLink.indexOf("/") + 2, rootLink.lastIndexOf("."));
+            try (FileWriter writer = new FileWriter(FOLDER_PATH + "/" + siteName + "Map.txt")) {
                 String mapString = MapCreator.createMap(node, 0);
                 writer.write(mapString);
             } catch (IOException e) {
